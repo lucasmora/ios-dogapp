0ccc97bc2d730fc27dc7e5af06aff20c1e0a0360
Lucas Môra <mora.lucas@live.com>
2023-10-24 20:46:13 -0300

    Initial Commit

---
+++ DogApp/DogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogAppApp.swift
+//  DogApp
+//
+//  Created by Lucas Môra on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
